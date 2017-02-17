449c4a1269ca7f3358a8b3aa4a24eafbcdf43d8d
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-02-17 17:47:37 -0500

    gathered all unique genes, initialized matrix

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -1,10 +1,48 @@
+import json_scripts
+import numpy as np
+import pandas as pd
+
 def main():
 
   merge_exp_sigs()
 
 def merge_exp_sigs():
+  load_sigs_to_json()
+
+  merge_sigs_to_mat()
+
+def merge_sigs_to_mat():
+  exp_sigs = json_scripts.load_to_dict('proc_data/exp_sigs.json')
+
+  num_sigs = len(exp_sigs.keys())
+
+  # collect all genes across all experimental signatures
+  all_genes = []
+
+  for sig_name in exp_sigs:
+
+    print(sig_name)
+
+    inst_sig = exp_sigs[sig_name]
+
+    for inst_gene in inst_sig:
+      all_genes.append(inst_gene)
+
+  print(len(all_genes))
+  all_genes = list(set(all_genes))
+  print(len(all_genes))
+
+  num_genes = len(all_genes)
+
+  print('there are ' + str(num_genes) + ' unique genes')
+
+  mat = np.zeros([num_genes, num_sigs])
+
+  print(mat.shape)
+
+
+def load_sigs_to_json():
   import glob
-  import json_scripts
 
   file_names = glob.glob('files_2-17-2017/hdf*.txt')
 
