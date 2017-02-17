64200455e409c62466ac75dabab316591a7787e8
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-02-17 17:39:56 -0500

    loaded sigs into exp_sigs.json, merging into matrix

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -4,13 +4,33 @@ def main():
 
 def merge_exp_sigs():
   import glob
+  import json_scripts
 
   file_names = glob.glob('files_2-17-2017/hdf*.txt')
 
+  # store all signatures in a dictionary
+  exp_sigs = {}
+
   for inst_filename in file_names:
+
+    inst_sig = inst_filename.split('.txt')[0].split('/')[1].split('_chdir')[0]
+
+    # initialize dictionary for signature
+    exp_sigs[inst_sig] = {}
 
     f = open(inst_filename, 'r')
     lines = f.readlines()
+
+    for inst_line in lines:
+      inst_line = inst_line.strip().split(',')
+
+      inst_gene = inst_line[0]
+      inst_value = inst_line[1]
+
+      exp_sigs[inst_sig][inst_gene] = inst_value
+
     f.close()
+
+  json_scripts.save_to_json(exp_sigs, 'proc_data/exp_sigs.json', indent='indent')
 
 main()
